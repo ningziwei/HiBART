eefe3c9a91203135307f931e3fc3bfaac12542ba
ningziwei <451372872@qq.com>
2022-10-08 11:15:45 +0800

    调整用于计算相似度的向量的计算位置

--- model/hi_bart.py
+++ model/hi_bart.py
@@ -19,8 +19,8 @@ class HiDecoder(nn.Module):
         )
     
     def forward(
-        self, enc_output, 
-        enc_src_ids, enc_src_len, enc_mask, 
+        self, enc_output, src_embed,
+        enc_src_len, enc_mask, 
         dec_src_ids, dec_mask):
         '''
         预测一次输出，得到token是每个候选token的概率
@@ -57,20 +57,34 @@ class HiDecoder(nn.Module):
             # print('57', hidden_state.shape, eos_emb.shape)
             eos_scores = torch.bmm(hidden_state, eos_emb.permute(0,2,1))
         
-        enc_src_embed = self.decoder.embed_tokens(enc_src_ids)
-        enc_src_embed = self.dropout_layer(enc_src_embed)
-        enc_output = self.encoder_mlp(enc_output)
-        enc_output = self.dropout_layer(enc_output)
-        src_embed = (enc_src_embed + enc_output)/2
+        # enc_src_embed = self.decoder.embed_tokens(enc_src_ids)
+        # enc_src_embed = self.dropout_layer(enc_src_embed)
+        # enc_output = self.encoder_mlp(enc_output)
+        # enc_output = self.dropout_layer(enc_output)
+        # src_embed = (enc_src_embed + enc_output)/2
         word_scores = torch.einsum('blh,bnh->bln', hidden_state, src_embed)
-        eos_scores = word_scores[range(len(enc_output)),:,enc_src_len-1]
-        eos_scores = eos_scores.unsqueeze(-1)
         
-        # word_scores: bsz*max_dec_len*max_enc_len
-        # enc_mask: bsz*1*max_enc_len, 结束标记的得分不算在word_score中
-        # dec_mask: bsz*max_dec_len*1
+        if self.args['static_eos']:
+            # 静态结束符，用词表中的结束符嵌入向量作为目标
+            eos_id = self.args['eos_id']
+            eos_emb = self.decoder.embed_tokens.weight[eos_id:eos_id+1]
+            eos_scores = F.linear(hidden_state, self.dropout_layer(eos_emb))
+        else:
+            # 动态结束符，用结束符的编码向量作为目标
+            # eos_emb = enc_output[range(len(enc_output)), enc_src_len-1, :]
+            # eos_emb = eos_emb.unsqueeze(1)
+            # eos_scores = torch.bmm(hidden_state, eos_emb.permute(0,2,1))
+            eos_scores = word_scores[range(len(enc_output)),:,enc_src_len-1]
+            eos_scores = eos_scores.unsqueeze(-1)
+        
+        '''
+        word_scores: bsz*max_dec_len*max_enc_len
+        enc_mask: bsz*1*max_enc_len
+        dec_mask: bsz*max_dec_len*1
+        '''
+        # 结束标记的得分不算在word_score中
+        enc_mask[range(len(enc_mask)), enc_src_len-1] = 0
         enc_mask = enc_mask.eq(0)
-        # enc_mask[range(len(enc_mask)), enc_src_len-1] = True
         enc_mask = enc_mask.unsqueeze(1)
         word_scores = word_scores.masked_fill(enc_mask, -1e32)
         # dec_mask = dec_mask.eq(0).unsqueeze(-1)
@@ -88,6 +102,13 @@ class HiBart(nn.Module):
     def __init__(self, bart, loss_fn, args):
         super(HiBart, self).__init__()
         self.encoder = bart.encoder
+        self.dropout_layer = nn.Dropout(0.3)
+        hidden_size = self.encoder.embed_tokens.weight.size(1)
+        self.encoder_mlp = nn.Sequential(
+            nn.Linear(hidden_size, hidden_size),
+            nn.Dropout(0.3), nn.ReLU(),
+            nn.Linear(hidden_size, hidden_size)
+        )
         self.hi_decoder = HiDecoder(bart.decoder, args)
         self.loss_fn = loss_fn
     
@@ -104,7 +125,8 @@ class HiBart(nn.Module):
 
     def forward(
         self, enc_src_ids, enc_src_len, enc_mask, 
-        dec_src_ids_bund, dec_mask_bund, dec_targ_pos_bund):
+        dec_src_ids_bund=None, dec_mask_bund=None, dec_targ_pos_bund=None
+    ):
         '''
         enc_src_ids: batch_size*enc_max_len
         enc_src_len: batch_size*1
@@ -118,23 +140,39 @@ class HiBart(nn.Module):
             input_ids=enc_src_ids, attention_mask=enc_mask, 
             return_dict=True, output_hidden_states=True,
         )
+        # print(enc_state_dic.keys())
         enc_output = enc_state_dic.last_hidden_state
-        dec_src_ids_bund = dec_src_ids_bund.permute(1,0,2)
-        dec_mask_bund = dec_mask_bund.permute(1,0,2)
-        dec_targ_pos_bund = dec_targ_pos_bund.permute(1,0,2)
-        # self.test(enc_output, 'hi 119')
+        # src_embed 用于在decoder输出部分计算相似度
+        enc_src_embed = self.encoder.embed_tokens(enc_src_ids)
+        enc_src_embed = self.dropout_layer(enc_src_embed)
+        enc_output_mlp = self.encoder_mlp(enc_output)
+        enc_output_mlp = self.dropout_layer(enc_output_mlp)
+        src_embed = (enc_src_embed + enc_output_mlp)/2
 
-        batch_loss = 0
-        for i in range(len(dec_targ_pos_bund)):
-            dec_src_ids = dec_src_ids_bund[i]
-            dec_mask = dec_mask_bund[i]
-            dec_targ_pos = dec_targ_pos_bund[i]
-            pred = self.hi_decoder(
-                enc_output, 
-                enc_src_ids, enc_src_len, enc_mask,
-                dec_src_ids, dec_mask)
-            batch_loss = self.loss_fn(
-                pred, dec_targ_pos, dec_mask)
+        if dec_targ_pos_bund is not None:
+            # 训练过程，三段训练依次进行，各loss求和后一起更新
+            dec_src_ids_bund = dec_src_ids_bund.permute(1,0,2)
+            dec_mask_bund = dec_mask_bund.permute(1,0,2)
+            dec_targ_pos_bund = dec_targ_pos_bund.permute(1,0,2)
+            batch_loss = 0
+            for i in range(len(dec_targ_pos_bund)):
+                dec_src_ids = dec_src_ids_bund[i]
+                dec_mask = dec_mask_bund[i]
+                dec_targ_pos = dec_targ_pos_bund[i]
+                logits = self.hi_decoder(
+                    enc_output, src_embed,
+                    enc_src_len, enc_mask,
+                    dec_src_ids, dec_mask)
+                batch_loss += self.loss_fn(
+                    logits, dec_targ_pos, dec_mask)
+        else:
+            # 预测过程，执行后解码，解码结果再给到decoder
+            input_ids=enc_src_ids
+            attention_mask=enc_mask
+            # for _ in range(3):
+
+
+
         
         return batch_loss
 
